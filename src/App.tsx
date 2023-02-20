077e2f7f8f866ab2c846eee545a9d4c00353852c
Alan Mortoni <mortoni.alan@hotmail.com>
2023-02-20 13:56:14 +1000

    feat: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import LoginForm from "./components/LoginForm";
+import Menu from "./components/Menu";
+import { MenuProps } from "./components/Menu/Menu";
+
+const data: MenuProps[] = [
+  {
+    id: "1",
+    label: "Menu 1",
+    items: [
+      {
+        id: "1-1",
+        label: "Menu 1-1",
+        items: [
+          {
+            id: "1-1-1",
+            label: "Menu 1-1-1",
+          },
+          {
+            id: "1-1-2",
+            label: "Menu 1-1-2",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "2",
+    label: "Menu 2",
+    items: [
+      {
+        id: "2-1",
+        label: "Menu 2-1",
+        items: [
+          {
+            id: "2-1-1",
+            label: "Menu 2-1-1",
+          },
+          {
+            id: "2-1-2",
+            label: "Menu 2-1-2",
+            items: [
+              {
+                id: "2-1-2-1",
+                label: "Menu 2-1-2-1",
+              },
+              {
+                id: "2-1-2-2",
+                label: "Menu 2-1-2-2",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 function App() {
+  const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
+  const [history, setHistory] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (selectedMenu) {
+      const isAlreadyInTheHistory = history.includes(selectedMenu);
+      if (!isAlreadyInTheHistory) {
+        setHistory((h) => [...h, selectedMenu]);
+      }
+    }
+  }, [selectedMenu, history]);
+
+  const handleSelectMenu = (menuId: string) => {
+    setSelectedMenu(menuId);
+  };
+
+  const handleCancel = useCallback(() => {
+    setSelectedMenu(history[history.length - 2]);
+    setHistory((h) =>
+      h
+        .slice(0, h.length - 1)
+        .filter((value, index) => h.indexOf(value) === index)
+    );
+  }, [history]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex space-x-10">
+      <Menu
+        menuData={data}
+        selectedMenu={selectedMenu}
+        onSelectMenu={handleSelectMenu}
+      />
+
+      <div>{selectedMenu && <LoginForm onCancel={handleCancel} />}</div>
     </div>
   );
 }
